fadd6db022313f3e7d46b93868a9c04bd1f1bc1b
therealroier <cuentaparatodoo0z@gmail.com>
2026-02-08 03:16:55 -0700

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,4 +1,21 @@
+const axios = require('axios');
 let usersDB = []; 
+
+const DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1469998225472880662/fQ8_e1mjU3-lFm-WU-qIbNTQ06CXjrXeiazo_o5uNPXBwWVCrI6w-SjDStPeBjCb5B11";
+const RAW_SCRIPT_URL = "https://pastefy.app/a5g4vwd3/raw";
+
+async function sendLog(title, message, color) {
+    try {
+        await axios.post(DISCORD_WEBHOOK, {
+            embeds: [{
+                title: title,
+                description: "```" + message + "```",
+                color: color,
+                timestamp: new Date()
+            }]
+        });
+    } catch (e) {}
+}
 
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -8,7 +25,7 @@ module.exports = async (req, res) => {
     if (req.method === 'OPTIONS') return res.status(200).end();
 
     const authHeader = req.headers['authorization'];
-    if (authHeader !== 'Bearer DZisthegoat') {
+    if (authHeader !== '260211') {
         return res.status(403).json({ message: "Forbidden" });
     }
 
@@ -17,7 +34,7 @@ module.exports = async (req, res) => {
         return res.status(404).send(''); 
     }
 
-    const { action, nickname, password, license, keyExpired } = req.body;
+    const { action, nickname, password, license, keyExpired, robloxName } = req.body;
 
     if (action === "register") {
         const exists = usersDB.find(u => u.nickname.toLowerCase() === nickname.toLowerCase());
@@ -25,12 +42,8 @@ module.exports = async (req, res) => {
             return res.status(400).json({ status: "error", message: "User already exists" });
         }
 
-        usersDB.push({
-            nickname,
-            password,
-            license
-        });
-
+        usersDB.push({ nickname, password, license });
+        await sendLog("New Registration", `Nick: ${nickname}\nLicense: ${license}\nRoblox: ${robloxName}`, 65280);
         return res.status(200).json({ status: "success" });
     }
 
@@ -43,12 +56,15 @@ module.exports = async (req, res) => {
 
         if (keyExpired) {
             usersDB.splice(userIndex, 1);
+            await sendLog("Account Deleted", `User: ${nickname}\nReason: Expired License`, 16711680);
             return res.status(410).json({ message: "Expired Key" });
         }
 
+        await sendLog("Login Success", `User: ${nickname}\nRoblox: ${robloxName}`, 255);
         return res.status(200).json({ 
             status: "success", 
-            license: usersDB[userIndex].license 
+            license: usersDB[userIndex].license,
+            scriptUrl: RAW_SCRIPT_URL
         });
     }
 
