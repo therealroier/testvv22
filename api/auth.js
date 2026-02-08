0595da51d141e0732f9973327cdce5fe3e0bed66
therealroier <cuentaparatodoo0z@gmail.com>
2026-02-08 03:43:41 -0700

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -1,34 +1,6 @@
-const https = require('https');
 let usersDB = []; 
 
-const DISCORD_WEBHOOK = "/api/webhooks/1469998225472880662/fQ8_e1mjU3-lFm-WU-qIbNTQ06CXjrXeiazo_o5uNPXBwWVCrI6w-SjDStPeBjCb5B11";
 const RAW_SCRIPT_URL = "https://pastefy.app/a5g4vwd3/raw";
-
-function sendLog(title, message, color) {
-    const data = JSON.stringify({
-        embeds: [{
-            title: title,
-            description: "```" + message + "```",
-            color: color,
-            timestamp: new Date()
-        }]
-    });
-
-    const options = {
-        hostname: 'discord.com',
-        port: 443,
-        path: DISCORD_WEBHOOK,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': data.length,
-        },
-    };
-
-    const req = https.request(options);
-    req.write(data);
-    req.end();
-}
 
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -47,7 +19,7 @@ module.exports = async (req, res) => {
         return res.status(404).send(''); 
     }
 
-    const { action, nickname, password, license, keyExpired, robloxName } = req.body;
+    const { action, nickname, password, license, keyExpired } = req.body;
 
     if (action === "register") {
         const exists = usersDB.find(u => u.nickname.toLowerCase() === nickname.toLowerCase());
@@ -56,7 +28,6 @@ module.exports = async (req, res) => {
         }
 
         usersDB.push({ nickname, password, license });
-        sendLog("New Registration", `Nick: ${nickname}\nLicense: ${license}\nRoblox: ${robloxName}`, 65280);
         return res.status(200).json({ status: "success" });
     }
 
@@ -69,11 +40,9 @@ module.exports = async (req, res) => {
 
         if (keyExpired) {
             usersDB.splice(userIndex, 1);
-            sendLog("Account Deleted", `User: ${nickname}\nReason: Expired License`, 16711680);
             return res.status(410).json({ message: "Expired Key" });
         }
 
-        sendLog("Login Success", `User: ${nickname}\nRoblox: ${robloxName}`, 255);
         return res.status(200).json({ 
             status: "success", 
             license: usersDB[userIndex].license,
