36f27dc8afe13ba88dda9ade9b565b873b7f4a1f
therealroier <cuentaparatodoo0z@gmail.com>
2026-02-08 02:47:24 -0700

    Remove CORS configuration comment
    Removed commented CORS configuration step.

--- api/auth.js
+++ api/auth.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-    // 1. Configurar CORS
+
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
